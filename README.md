4bbe1cd4d484ea681a9b6d8a4ddb8cb285eb899c
mattix1710 <grooteska@proton.me>
2025-12-12 00:38:31 +0100

    ✨ Add total frames probing

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 `ffprobe -v error -select_streams a:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 <input_file>`
 3. Probing for codec name:
 `ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 <input_file>`
+4. Probing for frame count:
+`ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 <input_file>`
--- processor/prober.go
+++ processor/prober.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 )
 
-func Get_bitrate(file string) int {
-	probe_args := []string{
+func GetBitrate(file string) int {
+	probeArgs := []string{
 		"ffprobe",
 		"-v", "error",
 		"-select_streams", "a:0",
@@ -17,7 +17,7 @@ func Get_bitrate(file string) int {
 		file,
 	}
 
-	cmd := exec.Command(probe_args[0], probe_args[1:]...)
+	cmd := exec.Command(probeArgs[0], probeArgs[1:]...)
 
 	var stderr strings.Builder
 	cmd.Stderr = &stderr
@@ -32,12 +32,12 @@ func Get_bitrate(file string) int {
 	}
 	bitrate := strings.TrimSpace(string(results))
 
-	bitrate_int, _ := strconv.Atoi(bitrate)
-	return bitrate_int / 1000
+	bitrateInt, _ := strconv.Atoi(bitrate)
+	return bitrateInt / 1000
 }
 
-func Get_codec(file string) string {
-	probe_args := []string{
+func GetCodec(file string) string {
+	probeArgs := []string{
 		"ffprobe",
 		"-v", "error",
 		"-select_streams", "a:0",
@@ -46,7 +46,7 @@ func Get_codec(file string) string {
 		file,
 	}
 
-	cmd := exec.Command(probe_args[0], probe_args[1:]...)
+	cmd := exec.Command(probeArgs[0], probeArgs[1:]...)
 
 	var stderr strings.Builder
 	cmd.Stderr = &stderr
@@ -61,6 +61,36 @@ func Get_codec(file string) string {
 	}
 	codec := strings.TrimSpace(string(results))
 	return codec
+}
+
+func GetTotalFrames(file string) int {
+	probeArgs := []string{
+		"ffprobe",
+		"-v", "error", // INFO: Hide "info" output (version info, etc.)
+		"-select_streams", "v:0", // INFO: Select only the first video stream
+		"-count_packets",                          // INFO: Count the number of packets per stream and report it in the corresponding stream section
+		"-show_entries", "stream=nb_read_packets", // INFO: Show only the entry for "nb_read_packets"
+		"-of", "csv=p=0", // INFO: Set the output formatting. In this case it hides the descriptions and only shows the value.
+		file,
+	}
+
+	cmd := exec.Command(probeArgs[0], probeArgs[1:]...)
+
+	var stderr strings.Builder
+	cmd.Stderr = &stderr
+
+	results, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Error running ffprobe: %v\n", err)
+		if stderr.Len() > 0 {
+			fmt.Printf("ffprobe stderr: %s\n", stderr.String())
+		}
+		return -1
+	}
+
+	framesCountStr := strings.TrimSpace(string(results))
+	framesCountInt, _ := strconv.Atoi(framesCountStr)
+	return framesCountInt
 }
 
 type PSNR struct {
